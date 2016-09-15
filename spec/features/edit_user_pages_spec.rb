e446b5208fbf215bc953f4c512103c0c0479820e
Julia Dickey and Lauren Posey <julia+lauren@>
2016-09-15 09:19:00 -0700

    Pass the edit profile integration spec

--- spec/features/edit_user_pages_spec.rb
+++ spec/features/edit_user_pages_spec.rb
@@ -3,13 +3,17 @@ require 'rails_helper'
 describe 'the edit user feature' do
   it 'edits a user' do
     user = FactoryGirl.create(:user)
-    visit images_path
+    visit '/'
+    click_on 'Login'
+    fill_in 'Email', :with => 'person@email.com'
+    fill_in 'Password', :with => 'password'
+    click_on 'Log in'
     click_link 'Edit profile'
     fill_in 'Email', :with => 'person@email.com'
     fill_in 'Password', :with => 'password'
     fill_in 'Password confirmation', :with => 'password'
     fill_in 'Current password', :with => 'password'
-    click_button 'Update'
+    click_on 'Update'
     expect(page).to have_content('Logged in as person@email.com')
   end
 end
